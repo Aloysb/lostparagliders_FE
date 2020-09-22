e6349fdecc99d6886f8b498023c27605d4b45240
Aloys Berger <aloysberger@gmail.com>
2020-09-22 15:26:53 +0200

    Add types definitions for react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <h1>Has this equipment been stolen?</h1>
+        <h2>
+          A website to find out if the paragliding equipment you are buying has
+          been stolen or not!
+        </h2>
+        <button></button>
       </header>
     </div>
   );
