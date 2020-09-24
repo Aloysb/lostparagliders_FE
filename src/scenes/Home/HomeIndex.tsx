20a1d86d308c84040ee36e444e8f3da171bdb66a
Aloys Berger <aloysberger@gmail.com>
2020-09-24 16:31:20 +0200

    Add logo to landing page

--- src/scenes/Home/HomeIndex.tsx
+++ src/scenes/Home/HomeIndex.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 //Components
 import Button from '../../components/Button';
 
+import Logo from '../../assets/logo/Logo.png';
+
 const Home = () => {
   return (
     <div className='flex justify-center items-center w-screen h-screen'>
       <div className='w-4/5'>
-        <img className='h-40 w-40 mx-auto bg-blue-900' alt='Website logo' />
+        <img className='h-40 mx-auto' alt='Website logo' src={Logo} />
         <h1 className='display text-center my-5'>
           Has this equipment been stolen?
         </h1>
