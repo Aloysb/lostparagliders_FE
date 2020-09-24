45fd5973e7b1983f75d4f7113959634545cc1c96
Aloys Berger <aloysberger@gmail.com>
2020-09-24 15:42:45 +0200

    Style button component
    Add hooks loading state
    
    Add spinner on loading

--- src/components/Button.stories.tsx
+++ src/components/Button.stories.tsx
@@ -8,5 +8,12 @@ export default {
 } as Meta;
 
 export const Primary: React.SFC<{}> = () => <Button variant='primary'></Button>;
-
 export const Warning: React.SFC<{}> = () => <Button variant='warning'></Button>;
+export const Success: React.SFC<{}> = () => <Button variant='success'></Button>;
+export const Info: React.SFC<{}> = () => <Button variant='info'></Button>;
+export const Disabled: React.SFC<{}> = () => (
+  <Button variant='disabled'></Button>
+);
+export const Example: React.SFC<{}> = () => (
+  <Button variant='warning' title="I'm buying equipment"></Button>
+);
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,27 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 //This is necessary to use tailwind with Styled components.
 import tw from 'tailwind.macro';
+//Spinners
+import { Ellipsis } from 'react-spinners-css';
 
 //Styled component.
 const StyledButton = styled.button<ButtonProps>`
-  @import url('https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
-  font-family: mulish;
-  background: ${(props) => props.variant};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1rem;
   color: white;
-  background-color: ${(props) =>
-    props.variant === 'primary' ? 'var(--success)' : 'red'};
-  letter-spacing: 120%;
+  background-color: ${(props) => `var(--${props.variant})`};
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.06),
+    0 1px 2px 0 ${(props) => `var(--${props.variant})`};
   border: none;
+  letter-spacing: 0.1rem;
+  font-variant: small-caps;
+  font-weight: 600;
+  cursor: pointer;
+  transition: 0.3s all ease-in-out;
+  min-height: 3rem;
   ${tw`
-  text-base
+  lowercase
   px-8
-  py-2
   rounded
-  uppercase
   my-2
   mx-2
   `};
+
+  &:hover {
+    transform: scale(1.01);
+    opacity: 0.8;
+  }
+
+  @keyframes click-animation {
+    0% {
+      box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.06),
+        0 1px 2px 0 ${(props) => `var(--${props.variant})`};
+    }
+    50% {
+      box-shadow: inset 0 2px 4px 0 rgba(0, 0, 0, 1);
+    }
+    100% {
+      box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.06),
+        0 1px 2px 0 ${(props) => `var(--${props.variant})`};
+    }
+  }
+
+  &:focus {
+    opacity: 1;
+    outline: none;
+    animation-name: click-animation;
+    animation-duration: 1s;
+  }
 `;
 
 //TS type definition for the props.
@@ -32,7 +65,15 @@ interface ButtonProps {
 
 // Component itself
 export const Button = (props: ButtonProps) => {
-  return <StyledButton variant={props.variant}>{props.title}</StyledButton>;
+  const [loading, setLoading] = useState(false);
+
+  const displayLoadingSpinner = () => setLoading(true);
+
+  return (
+    <StyledButton variant={props.variant} onClick={displayLoadingSpinner}>
+      {loading ? <Ellipsis size={30} color='#fff' /> : props.title}
+    </StyledButton>
+  );
 };
 
 // Default props for the component
