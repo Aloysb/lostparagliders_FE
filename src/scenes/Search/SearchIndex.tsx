6e0f5a209dc2b2286c396b7ce353faf18bcad09d
Aloys Berger <aloysberger@gmail.com>
2020-09-25 22:54:56 +0200

    Centering the elements on the scene Search

--- src/scenes/Search/SearchIndex.tsx
+++ src/scenes/Search/SearchIndex.tsx
@@ -4,11 +4,22 @@ import React from 'react';
 import Button from '../../components/Button/Button';
 import SearchInput from '../../components/SearchInput/SearchInput';
 
+import Logo from '../../assets/logo/Logo.png';
+
 const Search = () => {
   return (
-    <div>
-      <SearchInput />
-      <Button variant='warning' title='Search' />
+    <div className='flex justify-center items-center w-screen h-screen'>
+      <div className='w-4/5'>
+        <img className='h-40 mx-auto' alt='Website logo' src={Logo} />
+        <h1 className='display text-center my-5'>
+          Has this equipment been stolen?
+        </h1>
+        <h3 className='text-center my-3'>
+          Enter the serial number below to find out!
+        </h3>
+        <SearchInput />
+        <Button variant='warning' title='Search' />
+      </div>
     </div>
   );
 };
