a0cd501ff9404ff1d51bae77d2603b4f041e6404
Aloys Berger <aloysberger@gmail.com>
2020-09-25 23:05:58 +0200

    Add container Main - set it up with props types for TS

--- src/scenes/Search/SearchIndex.tsx
+++ src/scenes/Search/SearchIndex.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
 
+//Container
+import CenterContainer from '../../components/CenterContainer/CenterContainer';
+
 //Components
 import Button from '../../components/Button/Button';
 import SearchInput from '../../components/SearchInput/SearchInput';
 
-import Logo from '../../assets/logo/Logo.png';
-
 const Search = () => {
   return (
-    <div className='flex justify-center items-center w-screen h-screen'>
-      <div className='w-4/5'>
-        <img className='h-40 mx-auto' alt='Website logo' src={Logo} />
-        <h1 className='display text-center my-5'>
-          Has this equipment been stolen?
-        </h1>
-        <h3 className='text-center my-3'>
-          Enter the serial number below to find out!
-        </h3>
-        <SearchInput />
-        <Button variant='warning' title='Search' />
-      </div>
-    </div>
+    <CenterContainer>
+      <h1 className='display text-center my-5'>
+        Has this equipment been stolen?
+      </h1>
+      <h3 className='text-center my-3'>
+        Enter the serial number below to find out!
+      </h3>
+      <SearchInput />
+      <Button variant='warning' title='Search' />
+    </CenterContainer>
   );
 };
 
