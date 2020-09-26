a93585c83a583d23b063f1087c04bcbaca7317dc
Aloys Berger <aloysberger@gmail.com>
2020-09-26 22:07:13 +0200

    Rework Main container to help with consistency (logo not jumping around)

--- src/components/CenterContainer/CenterContainer.tsx
+++ src/components/CenterContainer/CenterContainer.tsx
@@ -9,10 +9,14 @@ interface ChildrenProps {
 
 const CenterContainer = (props: ChildrenProps) => {
   return (
-    <div className='flex justify-center items-center w-screen min-h-screen'>
-      <div className='w-4/5'>
+    <div className='min-h-screen flex flex-col'>
+      <div className='mx-auto pt-24'>
         <img className='h-40 mx-auto' alt='Website logo' src={Logo} />
-        {props.children}
+      </div>
+      <div className='flex-grow flex justify-center'>
+        <div className='w-4/5 flex flex-col items-center justify-start'>
+          {props.children}
+        </div>
       </div>
     </div>
   );
