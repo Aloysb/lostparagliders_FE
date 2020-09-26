bf11d302f44c8d0012e8f05a74267edfededdf9d
Aloys Berger <aloysberger@gmail.com>
2020-09-26 22:53:10 +0200

    Add page under construction for registring stolen gear

---
+++ src/scenes/RegisterStolenGear/RegisterStolenGear.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import CenterContainer from './../../components/CenterContainer/CenterContainer';
+
+const RegisterStolenGear = () => {
+  return (
+    <CenterContainer>
+      <h1 className='display text-center mt-12'>Page under construction.</h1>
+      <h3 className='text-center mt-6'>
+        We're working hard to get this tool online soon! Come back to us shortly
+        😃
+      </h3>
+    </CenterContainer>
+  );
+};
+
+export default RegisterStolenGear;
