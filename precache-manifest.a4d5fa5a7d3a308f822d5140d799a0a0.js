84c1507e525c31d0c381695d9bd3e51324cb5a94
Aloys Berger <aloysberger@gmail.com>
2020-09-29 15:22:13 +0200

    Updates

---
+++ precache-manifest.a4d5fa5a7d3a308f822d5140d799a0a0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b1e6ddd0fe8f23ebf2c1a5caa724971",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60bb456bf0406962fc48",
+    "url": "/static/css/2.079ed09a.chunk.css"
+  },
+  {
+    "revision": "76e6d6d493e7bcf19810",
+    "url": "/static/css/main.b1807335.chunk.css"
+  },
+  {
+    "revision": "60bb456bf0406962fc48",
+    "url": "/static/js/2.f3a2f92d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f3a2f92d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0036232e8f442a747a5a",
+    "url": "/static/js/3.e51cf637.chunk.js"
+  },
+  {
+    "revision": "9d2b9c54491870cb4db1",
+    "url": "/static/js/4.f881f83f.chunk.js"
+  },
+  {
+    "revision": "94ee1e8c1da3e1b362ef",
+    "url": "/static/js/5.6c07505c.chunk.js"
+  },
+  {
+    "revision": "76e6d6d493e7bcf19810",
+    "url": "/static/js/main.04d97fdf.chunk.js"
+  },
+  {
+    "revision": "8fa1507bb64b394364a6",
+    "url": "/static/js/runtime-main.d36073d5.js"
+  }
+]);
