061bef24f1cdcb14d6b75ad0b3c904935d45031c
Aloys Berger <aloysberger@gmail.com>
2020-09-29 14:58:17 +0200

    Updates

---
+++ precache-manifest.fb8a977a862ce5802f62fc4c3cc3fdd0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2972fbc230f808dfaf732d872afa6b0d",
+    "url": "/lostparagliders_FE/index.html"
+  },
+  {
+    "revision": "95e053fb03f1994afbbc",
+    "url": "/lostparagliders_FE/static/css/2.079ed09a.chunk.css"
+  },
+  {
+    "revision": "bd464e28e4b0e9df1d7b",
+    "url": "/lostparagliders_FE/static/css/main.b1807335.chunk.css"
+  },
+  {
+    "revision": "95e053fb03f1994afbbc",
+    "url": "/lostparagliders_FE/static/js/2.5fccd8b3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/lostparagliders_FE/static/js/2.5fccd8b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd464e28e4b0e9df1d7b",
+    "url": "/lostparagliders_FE/static/js/main.69c01985.chunk.js"
+  },
+  {
+    "revision": "531524489183ae2c02f5",
+    "url": "/lostparagliders_FE/static/js/runtime-main.b995ab88.js"
+  }
+]);
