aa8514408ec4f13ab8bfbc80c652cd16b2eefba5
Aloys Berger <aloysberger@gmail.com>
2020-09-29 15:47:09 +0200

    Fix issue with global style's background not appearing

--- src/style/styled_components/globalStyle.tsx
+++ src/style/styled_components/globalStyle.tsx
@@ -19,7 +19,7 @@ const GlobalStyle = createGlobalStyle`
     @import url('https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
     font-family: 'Mulish';
     background: var(--main-dark-2);
-    background: linear-gradient(to bottom, var(--main-dark-2), #141E30); 
+    background: linear-gradient(to bottom, var(--main-dark-2), #141E30);
   }
 
   .display{
